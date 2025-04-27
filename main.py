6e0faf79cf8b137d16aa6ecf729d8ce76be57c98
tikkytak <theforgeworks@gmail.com>
2025-04-27 13:41:44 +0200

    BookBot completed

--- main.py
+++ main.py
@@ -1,22 +1,32 @@
 from stats import word_count
 from stats import character_count
 from stats import sort
+import sys
 
-with open('books/frankenstein.txt') as f:
-        text = f.read()
-            
-dict = character_count(text)            
+
+
+         
 def main():
         
-        sorted_list = sort(dict)       
-        print("============ BOOKBOT ============")
-        print("Analyzing book found at books/frankenstein.txt...")
-        print("----------- Word Count ----------")
-        print(f"Found {word_count(text)} total words")
-        print("--------- Character Count -------")
-        for dictionary in sorted_list:
-                if dictionary["char"].isalpha():
-                        print(f"{dictionary["char"]}: {dictionary["num"]}")
-        print("============= END ===============")
+        if len(sys.argv) < 2:
+                print("Usage: python3 main.py <path_to_book>")
+                sys.exit(1)
+                
+        else:
+                with open(sys.argv[1]) as f:
+                        text = f.read()
+            
+                dict = character_count(text)   
+
+                sorted_list = sort(dict)       
+                print("============ BOOKBOT ============")
+                print(f"Analyzing book found at {sys.argv[1]}")
+                print("----------- Word Count ----------")
+                print(f"Found {word_count(text)} total words")
+                print("--------- Character Count -------")
+                for dictionary in sorted_list:
+                        if dictionary["char"].isalpha():
+                                print(f"{dictionary["char"]}: {dictionary["num"]}")
+                print("============= END ===============")
 
 main()
